ab39284006b730b50c9104f785e9cd4bc52d37e2
Oliver Stolpe <oliver.stolpe@charite.de>
2026-06-25 09:05:28 +0200

    fix: new mehari cons and query parameter.

--- src/api/mehari/client.ts
+++ src/api/mehari/client.ts
@@ -39,7 +39,7 @@ export class MehariClient {
     const { genomeBuild, chrom, pos, del, ins } = seqvar
     const hgncSuffix = hgncId ? `&hgnc_id=${hgncId}` : ''
     const url =
-      `${this.apiBaseUrl}/api/v1/seqvars/csq?genome_release=${genomeBuild}&` +
+      `${this.apiBaseUrl}/api/v1/seqvars/csq?assembly=${genomeBuild}&` +
       `chromosome=${chrom}&position=${pos}&reference=${del}&` +
       `alternative=${ins}${hgncSuffix}`
 
@@ -74,12 +74,12 @@ export class MehariClient {
     const { svType, genomeBuild, chrom, start } = strucvar
     if (svType === 'BND' || svType === 'INS') {
       url =
-        `${this.apiBaseUrl}/api/v1/strucvars/csq?genome_release=${genomeBuild}&` +
+        `${this.apiBaseUrl}/api/v1/strucvars/csq?assembly=${genomeBuild}&` +
         `chromosome=${chrom}&start=${start}&stop=${start + 1}&sv_type=${svType}`
     } else {
       const { stop } = strucvar
       url =
-        `${this.apiBaseUrl}/api/v1/strucvars/csq?genome_release=${genomeBuild}&` +
+        `${this.apiBaseUrl}/api/v1/strucvars/csq?assembly=${genomeBuild}&` +
         `chromosome=${chrom}&start=${start}&stop=${stop}&sv_type=${svType}`
     }
 
